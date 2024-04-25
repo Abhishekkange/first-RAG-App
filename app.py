667be16bf49e68625699cdd8230907926aae19bd
Abhishek <Abhishekkange@gmail.com>
2024-04-25 08:39:34 +0530

    created vector similiarity search using googlGenerativeAIEmbeddings

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import google.generativeai as genai
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain import PromptTemplate
 from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA 
+import os
+
+os.environ['GOOGLE_API_KEY'] = 'AIzaSyANqyvxOpFeKyVjDI_fbkTGp2aaKYu_MS4'
+
 
 
 def getTextFromPdf(pdfFile):
@@ -31,8 +36,16 @@ def split_text(text):
 
 def generateEmbeddings(chunkedText):
 
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key = "RAIzaSyANqyvxOpFeKyVjDI_fbkTGp2aaKYu_MS4Z")
+    print("generating embedding")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key = "AIzaSyANqyvxOpFeKyVjDI_fbkTGp2aaKYu_MS4")
     db = Chroma.from_texts(chunkedText, embeddings)
+    print("eembeddings generated")
+    return db
+
+def create_model():
+    llm = genai.GenerativeModel(model_name="gemini-pro")
+    return llm
+
 
 
 
@@ -48,7 +61,17 @@ def generateEmbeddings(chunkedText):
 # main
 allText = getTextFromPdf('abc.pdf')
 chunked_text = split_text(allText)
-generateEmbeddings(chunked_text)
+embeddings = generateEmbeddings(chunked_text)
+result = embeddings.similarity_search('In Rich Dad Poor Dad Robert Kiyosaki mentions his first business venture, which involved selling "Crushed Surfers," wallets made out of Velcro and nylon. He created these wallets with the help of his best friend Mike and started selling them to his classmates in high school.')
+print(result[0].page_content)
+# model = create_model()
+# qa_chain = RetrievalQA.from_chain_type(model,retriever=embeddings,return_source_documents = True)
+
+# question = "what did rich dad told to author?"
+# result = qa_chain({"query":question})
+# print(result)
+
+
 
 
 
